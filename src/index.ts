57811346c572d908a9717c885c0e873c4e4a868c
Gandara <120376565+Gandara247@users.noreply.github.com>
2023-04-03 09:27:24 -0300

    feat: removing unused files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 import http from "http";
-import express, {Express, Request, Response} from "express";
+import express,{ Express, Request, Response} from "express";
 import routes from "./routes";
+
+const router: Express = express();
+router.use(express.urlencoded({extended: false}));
+router.use(express.json());
+
+router.use((req: Request, res: Response, next: any) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With,Content-Type,Accept, Authorization");
+    if(req.method === "OPTIOPNS") {
+        res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+        return res.status(200).json({});
+    }
+    next()
+});
+
+router.use("/", routes)
+
+const httpServer = http.createServer(router);
+httpServer.listen(3000, () => console.log("Servidor conectado na porta 3000!"));
